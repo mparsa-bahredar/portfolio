5acde36d6ed997fc11426259b3161321540334f0
Parsa <mparsabahredar@gmail.com>
2026-08-11 13:24:14 +0330

    Work on UI.

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
     const t = useTranslations("footer");
 
     return (
-        <div className="flex justify-center mt-8 py-2 bg-[#EEEEEE]   dark:bg-[#]">
-            <p className=" text-[#A3A3A3]   dark:text-[#A3A3A3]">{t("copyRight")}</p>
+        <div className="flex justify-center mt-8 py-2 bg-[#EEEEEE]   dark:bg-[#071E31]">
+            <p className="text-[14px] text-[#A3A3A3]   dark:text-[#A3A3A3]">{t("copyRight")}</p>
         </div>
     )
 
